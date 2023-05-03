e720f47d4f9877b150f7c2c30dc7d8d044a88ce2
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-05-03 08:54:26 +0530

    footer added

--- assest/JS/main.js
+++ assest/JS/main.js
@@ -34,3 +34,52 @@ const tabs = document.querySelectorAll('[data-target]'),
     })
 
 // =================== CONTACT FORM ==================================
+const contactForm = document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactSubject = document.getElementById('contact-subject'),
+    contacMessage = document.getElementById('contact-message'),
+    errorMessage = document.getElementById('error-message');
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        //check field has value
+        if(contactName.value === '' || 
+            contactEmail.value === '' || 
+            contactSubject.value === '' || 
+            contacMessage.value === '' 
+        ){
+            // show msg
+            errorMessage.textContent = 'write all the input fields'
+        }else{
+            // serviceID -templateID -#from -publicey
+            emailjs.sendForm(
+                'service_8sz780b',
+                'template_w9ahpib',
+                '#contact-form',
+                'vEHtuNnTx0daZ9eqU'
+            ).then(() => {
+                //show message and add colour, window + dot to open emojy
+                errorMessage.classList.add('color-first') 
+                errorMessage.textContent = 'message send ✔ '
+
+                //remove msg after 5 second
+                setTimeout(() => {
+                    errorMessage.textContent = '';
+                },5000);
+            }, (error) => {
+                alert('OOPs! SOMETHIN WENT WRONG...', error);
+                }
+            );
+
+            //clear input fields
+            contactName.value = '' ;
+            contactEmail.value = '' ;
+            contactSubject.value = '' ;
+            contacMessage.value = '' ;
+        }
+    };
+
+    contactForm.addEventListener('submit', sendEmail);
+
