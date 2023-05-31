095227ea16c253bef7319a23040eb1bf58fbdaeb
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-05-31 13:56:35 +0530

    save customer & added to the table

--- Assignment/JS/SPA/posjs.js
+++ Assignment/JS/SPA/posjs.js
@@ -1,6 +1,12 @@
+//===================== MODULE ===========================
+
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
+
+
+
+
